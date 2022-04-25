9ec99832d650d65976ceb4500d80511921cfee93
BillionDolarCode <96139385+BillionDolarCode@users.noreply.github.com>
2022-04-25 16:07:24 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,6 +5,7 @@ class minecraft {
     file {'/opt/minecraft/minecraft_server.jar':
       ensure => file,
       source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+      before => Service['minecraft'],
     }
     package {'java':
       ensure => present,
@@ -18,7 +19,8 @@ class minecraft {
       source => 'puppet:///modules/minecraft/minecraft.service',
     }
     service {'minecraft':
-      ensure => running,
-      enable => true,
+      ensure  => running,
+      enable  => true,
+      require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
     }
   }
